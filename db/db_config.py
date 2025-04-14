2145296726c528fa1578020e178e6d0496037478
ethan shi <ethan.shi@gempoll.com>
2025-04-14 15:46:19 +0800

    refactor(db): 重置数据库配置信息
    - 清空了数据库连接的 host、user、password 和 database 字段
    - 这样做可能是为了增加代码的可移植性和安全性

--- db/db_config.py
+++ db/db_config.py
@@ -2,10 +2,10 @@
 import pymysql
 
 conn = pymysql.connect(
-    host='10.7.100.245',
-    user='root',
-    password='gempoll',
-    database="gempoll_tips",
+    host='',
+    user='',
+    password='',
+    database="",
     port=3306,
     charset='utf8mb4',
 )
